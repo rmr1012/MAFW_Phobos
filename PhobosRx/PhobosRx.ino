8ad25033243f98360278c1fef713accbd9b9422c
Dennis Ren <rmr1012@gmail.com>
2020-01-26 15:00:17 -0800

    multistage single byte protocal working

--- PhobosRx/PhobosRx.ino
+++ PhobosRx/PhobosRx.ino
@@ -1,86 +1,171 @@
+#define F_CPU 1000000UL
+#define BAUD_PRESCALER 0
 
-#define SV1 0x18
-#define SV2 0x81
-//states
-#define LISTEN 0
-#define ADDR 1
-#define CMDRX 2
-#define CKSUM 3
-#define TALK 4
-
-#define RX_ACK 0x00
-
-#define CMD_ADDRPRG 0x55
 
 // === IO DEFINATIONS ====
 #define HBLED 4
 #define INTLED 5
-#define GPIO3 6
-#define GPIO4 7
+#define GPIO4 6
+#define GPIO3 7
+#define GPIO1 11
+#define GPIO2 10
+#define TXD 1 
+#define CMD_PING 0x0f
+#define CK_CMD_PING(data) (CMD_PING==(data&CMD_PING) ? 1:0)
+#define CMD_ACK 0x01
+#define CK_CMD_ACK(data) (CMD_ACK==(data&CMD_ACK) ? 1:0)
+#define CMD_RST 0x02
+#define CK_CMD_RST(data) (CMD_RST==(data&CMD_RST)) ? 1:0)
+#define CMD_ENUM 0x03
+#define CK_CMD_ENUM(data) (CMD_ENUM==(data&CMD_ENUM) ? 1:0)
+#define CMD_ARM 0x03
+#define CK_CMD_ARM(data) (CMD_ARM==(data&CMD_ARM) ? 1:0)
+#define CMD_CONT 0x04// continue enum
+#define CK_CMD_CONT(data) (CMD_CONT==(data&CMD_CONT) ? 1:0)
 
-int stage_id=0;
+#define TxPack(TxADDR,TxCMD) ((char)(TxADDR<<4)+TxCMD)
 
-int rx_state=LISTEN;
+#define SID(RxByte)  ((RxByte&0xf0)>>4)
+#define ForMe(RxByte) (SID(RxByte) == stage_id ? 1:0)
 
-int addr=0x00;
+#define _NOP() do { __asm__ __volatile__ ("nop"); } while (0)
+static volatile char Eight_Bit_ADC_Value;
+char stage_id=0;
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+
+
+void initialize_ADC(void)
+{
+  ADCSRA |= 1 << ADPS2;
+  ADMUX |= 1 << ADLAR | 1 << MUX1 | 1 << MUX0;
+  ADCSRA |= 1 << ADIE;
+  ADCSRA |= 1 << ADEN;
+  //  sei();
+  ADCSRA |= 1 << ADSC;
+}
+
+void initialize_UART(void)
+{
+  LINCR = 0;
+  LINBTR |= _BV(LDISR) | 1 << LBT4;
+  LINBTR &= ~(1 << LBT5);
+  LINBTR &= ~(1 << LBT3);
+  LINBTR &= ~(1 << LBT2);
+  LINBTR &= ~(1 << LBT1);
+  LINBTR &= ~(1 << LBT0);
+  
+  LINBRRH = (BAUD_PRESCALER >> 8);
+  LINBRRL = BAUD_PRESCALER;
+  
+  LINCR |= 1 << LENA | 0 << LCMD0 | 1 << LCMD1 | 1 << LCMD2;
+}
+
+void initialize_GPIO(void){
   pinMode(HBLED, OUTPUT);
-  Serial.begin(230400*4);//1Mhz UART
-  LINBRRH=0x00;
-  LINBRRL=0x00;
+  digitalWrite(HBLED, HIGH);
+  
+  pinMode(INTLED, OUTPUT);
+  digitalWrite(INTLED, HIGH);
+  
+  pinMode(GPIO1, INPUT);  
+  pinMode(GPIO2, INPUT);
+
+    pinMode(GPIO3, OUTPUT);
+  digitalWrite(GPIO3, LOW);
+  
+  pinMode(GPIO4, OUTPUT);
+  digitalWrite(GPIO4, LOW);
+
+  
+}
+void txen(int en){
+  if(en==1)
+    LINCR |= 1 << LCMD0;
+  else{
+    LINCR &= ~(1<< LCMD0);
+    pinMode(TXD, INPUT);
+  }
+}
+void sendByte(char c){
+  txen(1);
+  LINDAT=c;
+  while (LINSIR & (1 << LBUSY)); 
+  txen(0);
+}
+
+char recieved_data=0x00;
+int heartrate=1000;
+int HBstate=1;
+int main(void)
+{ 
   CLKPR=0x80;// the tool writes fucked out prescaler, restoring clock to full speed
   CLKPR=0x00;
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(HBLED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(250);                       // wait for a second
-  digitalWrite(HBLED, LOW);    // turn the LED off by making the voltage LOW
-  delay(250);                       // wait for a second
-}
-
-void serialEvent() {
-  unsigned char command=0x00;
-  unsigned char command_candidate=0x00;
-  unsigned char addr_candidate=0x00;
-  unsigned char stateReg[2] ={0x00,0x00};
-  while (Serial.available()) {
-    // get the new byte:
-    unsigned char inChar = (unsigned char)Serial.read();
-    if(rx_state==LISTEN){
-      stateReg[1]=stateReg[0];
-      stateReg[0]=inChar;
-    }
-    if (stateReg[1]==SV1 && stateReg[0]==SV2 && rx_state==LISTEN){
-      rx_state=ADDR;
-    }
-    else if (rx_state==ADDR){
-      rx_state=CMDRX;
-      addr_candidate=inChar;
-    }
-    else if (rx_state==CMDRX){
-      rx_state=CKSUM;
-      command_candidate=inChar;
-    }
-    else if (rx_state==CKSUM){
-      unsigned char sum=command_candidate+stateReg[1]+stateReg[0];
-      if (sum==inChar){
-        command=command_candidate;
-        if(!digitalRead(1) && addr==0 && command==CMD_ADDRPRG){
-          addr=addr_candidate;
+  DDRA |= (1 << PINA4) | (1 << PINA5) | (1 << PINA6) | (1 << PINA7)  ;
+  LINENIR=0x03;
+  
+//  PCICR=0x02; // pcint enable
+//  PCMSK1=0x0c; // pcint enable
+  sei();
+  initialize_UART();
+  //initialize_ADC();
+  initialize_GPIO();
+  int hrcnt=0;
+  while(digitalRead(GPIO1)==0);
+  sendByte(TxPack(stage_id,CMD_PING));
+//  LINDAT=0x59;
+  //wait untill I1 goes high
+  char rxbuf=0;
+  int effHr,hrbeeps;
+  while(1)
+  {
+    for(hrbeeps=0;hrbeeps<=2*stage_id+1;hrbeeps++){
+      if(hrbeeps>=2*stage_id+1)
+          effHr=heartrate;
+      else
+          effHr=heartrate/stage_id;
+      ////// ^HR control, letting LED indicate which stage it is
+      for(hrcnt=0;hrcnt<effHr;hrcnt++){
+        //_NOP();
+        if(recieved_data!=0x00){
+          rxbuf=recieved_data;
+          recieved_data=0x00;
+          if(CK_CMD_ENUM(rxbuf) && (stage_id==0)){
+         // if((recieved_data==0x13) && (stage_id==0)){
+            digitalWrite(INTLED,LOW);
+            stage_id=SID(rxbuf);
+            sendByte(TxPack(stage_id,CMD_ACK));
+            
+            
+          }
+          else if(CK_CMD_CONT(rxbuf) && ForMe(rxbuf)){// process cont command
+            digitalWrite(GPIO3,HIGH);
+          }
+//          else if(CK_CMD_RST(rxbuf) && ForMe(rxbuf)){
+//            
+//          }
+          else if(CK_CMD_PING(rxbuf) && ForMe(rxbuf)){
+            sendByte(TxPack(stage_id,CMD_ACK));
+          }
+         
         }
       }
-      replyAck();
-        
-      } 
-      rx_state==LISTEN;
+      digitalWrite(HBLED,!digitalRead(HBLED));
     }
+//    digitalWrite(GPIO3,!digitalRead(GPIO3));
+//    digitalWrite(GPIO4,!digitalRead(GPIO4));
+
+
   }
- void replyAck(){
-  Serial.print(addr);
-  Serial.print(RX_ACK);
- }
+
+}
+
+ISR (LIN_TC_vect)
+{ 
+  recieved_data = LINDAT;      
+}
+
+ISR(ADC_vect)
+{
+  Eight_Bit_ADC_Value = ADCH;
+  ADCSRA |= 1 << ADSC;
+}
