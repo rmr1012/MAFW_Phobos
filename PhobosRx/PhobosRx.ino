44bbd929eae53b418f6c4876de020bb0c790317b
Dennis Ren <rmr1012@gmail.com>
2020-01-27 21:33:08 -0800

    ahh pcint broke it

--- PhobosRx/PhobosRx.ino
+++ PhobosRx/PhobosRx.ino
@@ -1,8 +1,9 @@
+#include <avr/wdt.h> 
 #define F_CPU 1000000UL
 #define BAUD_PRESCALER 0
 
 
-// === IO DEFINATIONS ====
+// === CONTROL IO DEFINATIONS ====
 #define HBLED 4
 #define INTLED 5
 #define GPIO4 6
@@ -10,29 +11,52 @@
 #define GPIO1 11
 #define GPIO2 10
 #define TXD 1 
+
+// ==  STAGE IO DEFINATIONS == 
+#define TRIGGER1 8
+#define TRIGGER2 9
+#define OUTN 13
+#define OUTP 12
+#define ICOIL 3
+
 #define CMD_PING 0x0f
-#define CK_CMD_PING(data) (CMD_PING==(data&CMD_PING) ? 1:0)
+#define CK_CMD_PING(data) (CMD_PING==(data&0x0f) ? 1:0)
 #define CMD_ACK 0x01
-#define CK_CMD_ACK(data) (CMD_ACK==(data&CMD_ACK) ? 1:0)
+#define CK_CMD_ACK(data) (CMD_ACK==(data&0x0f) ? 1:0)
 #define CMD_RST 0x02
-#define CK_CMD_RST(data) (CMD_RST==(data&CMD_RST)) ? 1:0)
+#define CK_CMD_RST(data) (CMD_RST==(data&0x0f) ? 1:0)
 #define CMD_ENUM 0x03
-#define CK_CMD_ENUM(data) (CMD_ENUM==(data&CMD_ENUM) ? 1:0)
+#define CK_CMD_ENUM(data) (CMD_ENUM==(data&0x0f) ? 1:0)
 #define CMD_ARM 0x03
-#define CK_CMD_ARM(data) (CMD_ARM==(data&CMD_ARM) ? 1:0)
+#define CK_CMD_ARM(data) (CMD_ARM==(data&0x0f) ? 1:0)
 #define CMD_CONT 0x04// continue enum
-#define CK_CMD_CONT(data) (CMD_CONT==(data&CMD_CONT) ? 1:0)
+#define CK_CMD_CONT(data) (CMD_CONT==(data&0x0f) ? 1:0)
 
 #define TxPack(TxADDR,TxCMD) ((char)(TxADDR<<4)+TxCMD)
 
 #define SID(RxByte)  ((RxByte&0xf0)>>4)
-#define ForMe(RxByte) (SID(RxByte) == stage_id ? 1:0)
+#define ForMe(RxByte) (((SID(RxByte) == stage_id) | (SID(RxByte) == 0)) ? 1:0)
 
 #define _NOP() do { __asm__ __volatile__ ("nop"); } while (0)
+
+/// GLOBAL VARIABLES
 static volatile char Eight_Bit_ADC_Value;
-char stage_id=0;
+char stage_id;
+char recieved_data;
+int heartrate;
+int HBstate;
+char lastPB;
 
 
+// Function Pototype
+void wdt_init(void) __attribute__((naked)) __attribute__((section(".init3")));
+// Function Implementation
+void wdt_init(void)
+{
+    MCUSR = 0;
+    wdt_disable();
+    return;
+}
 
 void initialize_ADC(void)
 {
@@ -58,6 +82,8 @@ void initialize_UART(void)
   LINBRRL = BAUD_PRESCALER;
   
   LINCR |= 1 << LENA | 0 << LCMD0 | 1 << LCMD1 | 1 << LCMD2;
+    LINENIR=0x03;
+
 }
 
 void initialize_GPIO(void){
@@ -70,13 +96,20 @@ void initialize_GPIO(void){
   pinMode(GPIO1, INPUT);  
   pinMode(GPIO2, INPUT);
 
-    pinMode(GPIO3, OUTPUT);
+  pinMode(GPIO3, OUTPUT);
   digitalWrite(GPIO3, LOW);
   
   pinMode(GPIO4, OUTPUT);
   digitalWrite(GPIO4, LOW);
 
+// configure stage interface 
+  pinMode(OUTN, OUTPUT);
+  digitalWrite(OUTN, HIGH);
+  pinMode(OUTP, OUTPUT);
+  digitalWrite(OUTP, LOW);
   
+  pinMode(TRIGGER1, INPUT);
+  pinMode(TRIGGER2, INPUT);
 }
 void txen(int en){
   if(en==1)
@@ -93,39 +126,53 @@ void sendByte(char c){
   txen(0);
 }
 
-char recieved_data=0x00;
-int heartrate=1000;
-int HBstate=1;
+void initGlobalVars(){
+  stage_id=0;
+  recieved_data=0x00;
+  heartrate=2000;
+  HBstate=1;
+  lastPB=0x00;
+}
+
+void initPCINT(){
+  PCICR|= 1<<PCIE1; // pcint enable, bank 1 aka port b
+  PCMSK1|= 1<<PCINT0 | 1<<PCINT1; // pcint active pins Pb0 Pb1
+}
 int main(void)
 { 
-  CLKPR=0x80;// the tool writes fucked out prescaler, restoring clock to full speed
+  RSTLBL:initGlobalVars();
+  CLKPR=0x80;// the tool writes fucked up prescaler, restoring clock to full speed
   CLKPR=0x00;
-  DDRA |= (1 << PINA4) | (1 << PINA5) | (1 << PINA6) | (1 << PINA7)  ;
-  LINENIR=0x03;
-  
-//  PCICR=0x02; // pcint enable
-//  PCMSK1=0x0c; // pcint enable
+
   sei();
   initialize_UART();
   //initialize_ADC();
   initialize_GPIO();
+//  initPCINT();
+
   int hrcnt=0;
-  while(digitalRead(GPIO1)==0);
-  sendByte(TxPack(stage_id,CMD_PING));
-//  LINDAT=0x59;
-  //wait untill I1 goes high
   char rxbuf=0;
   int effHr,hrbeeps;
+  char SWRESET=false;
+  
+  while(digitalRead(GPIO1)==0);
+  sendByte(TxPack(stage_id,CMD_PING));
+  //wait untill I1 goes high
+
+  wdt_enable(WDTO_60MS);
   while(1)
   {
     for(hrbeeps=0;hrbeeps<=2*stage_id+1;hrbeeps++){
       if(hrbeeps>=2*stage_id+1)
           effHr=heartrate;
       else
-          effHr=heartrate/stage_id;
+          effHr=heartrate/stage_id/2;
       ////// ^HR control, letting LED indicate which stage it is
+      
       for(hrcnt=0;hrcnt<effHr;hrcnt++){
         //_NOP();
+        if(~SWRESET)
+          wdt_reset();//feeding WDT 
         if(recieved_data!=0x00){
           rxbuf=recieved_data;
           recieved_data=0x00;
@@ -133,19 +180,28 @@ int main(void)
          // if((recieved_data==0x13) && (stage_id==0)){
             digitalWrite(INTLED,LOW);
             stage_id=SID(rxbuf);
-            sendByte(TxPack(stage_id,CMD_ACK));
-            
-            
+            sendByte(TxPack(stage_id,CMD_ACK));   
           }
           else if(CK_CMD_CONT(rxbuf) && ForMe(rxbuf)){// process cont command
             digitalWrite(GPIO3,HIGH);
+            delayMicroseconds(50);
+            digitalWrite(GPIO3,HIGH);
           }
-//          else if(CK_CMD_RST(rxbuf) && ForMe(rxbuf)){
-//            
-//          }
+          else if(CK_CMD_RST(rxbuf) && ForMe(rxbuf)){
+//            rxbuf=0x00;
+//            recieved_data;
+//            LINENIR=0x00;
+//            while(1);//let code die here untill resets
+              wdt_disable();
+              initialize_GPIO();
+              delayMicroseconds(50);
+              goto RSTLBL;
+          }
           else if(CK_CMD_PING(rxbuf) && ForMe(rxbuf)){
             sendByte(TxPack(stage_id,CMD_ACK));
           }
+
+          rxbuf=0x00;
          
         }
       }
@@ -157,6 +213,30 @@ int main(void)
 
   }
 
+}
+
+ISR(PCINT0_vect){
+  char newPB=PINB;
+  if((((lastPB^newPB)&0x03) == 0x01) & ((newPB&0x03) ==0x01))//T1 went high
+  {
+    digitalWrite(OUTN,LOW);
+  }
+  else if((((lastPB^newPB)&0x03) == 0x01) & ((newPB&0x03) ==0x00))// T1 went low
+  {
+    digitalWrite(OUTN,HIGH);
+  }
+  else if((((lastPB^newPB)&0x03) == 0x02) & ((newPB&0x03) ==0x02))// T2 went high
+  {
+    digitalWrite(OUTP,LOW);
+  }
+  else if((((lastPB^newPB)&0x03) == 0x02) & ((newPB&0x03) ==0x00))// T2 went low
+  {
+    digitalWrite(OUTP,HIGH);
+  }
+  else{// nothing changed
+      return;
+  } 
+  lastPB=newPB; 
 }
 
 ISR (LIN_TC_vect)
